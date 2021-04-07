1cff8033c837ff58c05270ca99496bd3b423c18c
bbpp222006 <34976166+bbpp222006@users.noreply.github.com>
2021-04-07 14:10:35 +0800

    update Logjam Attack

--- login.py
+++ login.py
@@ -6,46 +6,42 @@ from libhustpass import main
 import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import requests
-requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
-requests.packages.urllib3.disable_warnings()
-
-# options = webdriver.ChromeOptions()
-# options.add_argument("--headless")
-# options.add_argument('--no-sandbox')
-# options.add_argument('--disable-gpu')
-# options.add_argument('--disable-dev-shm-usage')
-# options.add_experimental_option('prefs',{
-#     'credentials_enable_service': False,
-#     'profile': {
-#         'password_manager_enabled': False,
-#     },
-#     "download": {
-#         'default_directory': "/usr/local/download",
-#     }
-# })
-
-# driver = webdriver.Chrome(options=options)
-# # driver = webdriver.Chrome()
-
-# for i in range(3):
-#     ticket = main.doLogin(os.environ['USERNAME'],os.environ['PASSWORD'],"http://access.hust.edu.cn/IDKJ-P/P/studentApi")
-#     try:
-#         driver.get(ticket)
-#         break
-#     except Exception:
-#         print ("gg",i)
 
 
-# time.sleep(1)
-# driver.find_element_by_class_name("am-button").click()
-# time.sleep(1)
-# driver.find_element_by_class_name("textArea").send_keys("吃饭")
-# time.sleep(1)
-# driver.find_element_by_class_name("submitbtn").click()
-# time.sleep(5)
-# print (driver.title)
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
+options.add_experimental_option('prefs',{
+    'credentials_enable_service': False,
+    'profile': {
+        'password_manager_enabled': False,
+    },
+    "download": {
+        'default_directory': "/usr/local/download",
+    }
+})
 
-r = requests.session()
-login_html = r.get("http://access.hust.edu.cn/IDKJ-P/P/studentApi")
-print(login_html.text)
+driver = webdriver.Chrome(options=options)
+# driver = webdriver.Chrome()
+
+for i in range(3):
+    ticket = main.doLogin(os.environ['USERNAME'],os.environ['PASSWORD'],"http://access.hust.edu.cn/IDKJ-P/P/studentApi")
+    try:
+        driver.get(ticket)
+        break
+    except Exception:
+        print ("gg",i)
+
+
+time.sleep(1)
+driver.find_element_by_class_name("am-button").click()
+time.sleep(1)
+driver.find_element_by_class_name("textArea").send_keys("吃饭")
+time.sleep(1)
+driver.find_element_by_class_name("submitbtn").click()
+time.sleep(5)
+print (driver.title)
+
+
